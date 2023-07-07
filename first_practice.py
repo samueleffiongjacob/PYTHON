466f281217b6a63639d65efc9ac435d95c14b951
samueleffiongjacob <samueleffiong685@gmail.com>
2023-07-07 10:31:06 +0100

    MY FIRST WORK WITH PYTHON

---
+++ first_practice.py
@@ -0,0 +1,8 @@
+"' A COnverter program works well'"
+
+print("How many kilometer did you run today ?") # Asking a qustion
+kms = input() # collecting user input
+mile = float(kms)/1.60934 # operation to coverte into miles 
+mile = round(mile, 2) # round up miles to 2 decimal place
+
+print(f"Your  {kms} kilometers ride was {mile} mi ") # final out 
