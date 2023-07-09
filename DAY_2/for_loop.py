a3177e542e2bcc67ce7d9a322c541fd4985087f6
samueleffiongjacob <samueleffiong685@gmail.com>
2023-07-09 09:09:32 +0100

    LOOPS IN DAY2 AND GAME PRACTICE IN DAY1

---
+++ DAY_2/for_loop.py
@@ -0,0 +1,18 @@
+"""
+    what is a loop: a loop is form of automating things aor repeating things
+    what is varible in loop : the name or value you would print out
+    what is Litreable in loop : The function or variable the loop goes
+    through to produce result for loop
+
+    what for loop : a for loop is an operation of loop for a
+    particular function
+"""
+
+for char in "hello":
+    print(char)
+
+for char in "hello":
+    print(char * 8)
+
+for item in range(1, 20):
+    print(item)
