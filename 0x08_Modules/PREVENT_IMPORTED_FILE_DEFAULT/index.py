194ce05d430174bc03860e01a297f090f77d42b8
samueleffiongjacob <samueleffiong685@gmail.com>
2023-09-02 11:11:05 +0100

    MODULES

---
+++ 0x08_Modules/PREVENT_IMPORTED_FILE_DEFAULT/index.py
@@ -0,0 +1,6 @@
+def say_sup():
+    print(f"SUP! __My name__ is {__name__}")
+
+
+if __name__ == "__main__":
+    say_sup()
---
+++ 0x08_Modules/PREVENT_IMPORTED_FILE_DEFAULT/main.py
@@ -0,0 +1,9 @@
+from index import say_sup
+
+
+def say_Hi():
+    print(f"HI! My __name__ is {__name__}")
+
+
+say_sup()
+say_Hi()
